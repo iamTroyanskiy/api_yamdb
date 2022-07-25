4a371501df8c9ca43cf2b6863c3bd4d7bd5b3fea
engoren <100416626+engoren@users.noreply.github.com>
2022-07-25 18:12:26 +0300

    Comment, Review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from django.urls import include, path
-from rest_framework.routers import SimpleRouter
-
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
-from .views import SignupView, GetAuthTokenView, UsersViewSet
-
-app_name = 'api_auth'
-
-router = SimpleRouter()
-router.register(r'users', UsersViewSet)
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('genres', GenreViewSet, basename='genres')
-router.register('titles', TitleViewSet, basename='titles')
-
-
-urlpatterns = [
-    path('auth/signup/', SignupView.as_view()),
-    path('auth/token/', GetAuthTokenView.as_view()),
-    path('', include(router.urls))
-]
-=======
 from django.urls import path, include
 from rest_framework import routers
 
@@ -35,4 +13,3 @@ router_v1.register(
     ReviewViewSet,
     basename='reviews',
 )
->>>>>>> b22e124 (Comment, Review)
